00bc70a4b2c023a3cc4bcc5d0922ab893d9f0e7c
borden jardine <bordenjardine@gmail.com>
2013-11-09 15:53:45 -0500

    models

---
+++ app/models/artist_instrument.rb
@@ -0,0 +1,4 @@
+class ArtistInstrument < ActiveRecord::Base
+	:validates_presence_of :artist_id
+	:validates_presence_of :instrument_id
+end
---
+++ app/models/instrument.rb
@@ -0,0 +1,3 @@
+class Instrument < ActiveRecord::Base
+	:validates_presence_of :name
+end
