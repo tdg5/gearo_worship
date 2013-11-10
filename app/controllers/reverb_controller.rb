b7185b1615861b3ecc448ca14c5a601c54a6287d
Tristan Crockett <thcsquad@gmail.com>
2013-11-10 02:18:26 -0500

    Return empty array

--- app/controllers/reverb_controller.rb
+++ app/controllers/reverb_controller.rb
@@ -7,7 +7,7 @@ class ReverbController < ApplicationController
 	def artist
 		artist = Artist.find_by(name: params[:artist_name])
 		if artist.nil?
-			return render json: ''
+			return render json: []
 		else
 			return render json: ReverbRequest.kickoff_request(artist.id)
 		end
