5b60f530a55c56438f368eca22a4a10c9755eed5
Tristan Crockett <thcsquad@gmail.com>
2013-11-10 02:24:16 -0500

    Another place

--- app/controllers/reverb_controller.rb
+++ app/controllers/reverb_controller.rb
@@ -16,7 +16,7 @@ class ReverbController < ApplicationController
 
 	def reverb_request
 		still_to_come = ReverbRequestInstrument.where(:request_id => params[:reverb_request_id], :completed => false)
-		return render json: '' if still_to_come.count > 0
+		return render json: [] if still_to_come.count > 0
 		reverb_responses = ReverbResponse.where(request_id: params[:reverb_request_id])
 		json_structure = reverb_responses.map { |reverb_response| 
 			instrument = Instrument.find(reverb_response.instrument_id)
